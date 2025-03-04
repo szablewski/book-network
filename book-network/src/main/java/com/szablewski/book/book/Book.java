37dcc937f8a07f3998b400c871dca2aa8e07a5d8
Bartosz Szablewski <bartosz@szablewski.hub.pl>
2025-03-04 21:52:47 +0100

    Adding entities and relation

---
+++ book-network/src/main/java/com/szablewski/book/book/Book.java
@@ -0,0 +1,45 @@
+package com.szablewski.book.book;
+
+import com.szablewski.book.common.BaseEntity;
+import com.szablewski.book.feedback.Feedback;
+import com.szablewski.book.history.BookTransactionHistory;
+import com.szablewski.book.user.User;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+import java.util.List;
+
+@Getter
+@Setter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Book extends BaseEntity {
+
+    private String title;
+    private String authorName;
+    private String isbn;
+    private String synopsis;
+    private String bookCover;
+    private boolean archived;
+    private boolean shareable;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
+    private User owner;
+
+    @OneToMany(mappedBy = "book")
+    private List<Feedback> feedbacks;
+
+    @OneToMany(mappedBy = "book")
+    private List<BookTransactionHistory> histories;
+
+}
