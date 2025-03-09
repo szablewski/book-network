126459f8fa37834683288f704b1c879e66c52a64
Bartosz Szablewski <bartosz@szablewski.hub.pl>
2025-03-09 09:07:11 +0100

    openApi config

---
+++ book-network/src/main/java/com/szablewski/book/config/OpenApiConfig.java
@@ -0,0 +1,52 @@
+package com.szablewski.book.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(
+        info = @Info(
+                contact = @Contact(
+                        name = "Bartek",
+                        email = "szablewski@bartosz.hub.pl"
+                ),
+                description = "OpenApi  documentation for spring security",
+                title = "Open specification - Bartek",
+                version = "1.0",
+                license = @License(
+                        name = "Licence name",
+                        url = "Licence url"
+                )
+        ),
+        servers = {
+                @Server(
+                      description = "Local ENV",
+                      url = "http://localhost:8088/api/v1"
+                ),
+                @Server(
+                        description = "Prod ENV",
+                        url = "https://prod/api/v1"
+                )
+        },
+        security = {
+                @SecurityRequirement(
+                        name = "bearerAuth"
+                )
+        }
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        description = "JWT aut desc",
+        scheme = "bearer",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+)
+class OpenApiConfig {
+}
